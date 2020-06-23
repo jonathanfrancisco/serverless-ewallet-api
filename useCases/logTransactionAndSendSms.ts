d183f9358e76e986feeb37a41d42cbceb90866e3
Jonathan Buted Francisco <jonathanfrancisco999@gmail.com>
2020-06-23 09:47:35 +0800

    update

--- useCases/logTransactionAndSendSms.ts
+++ useCases/logTransactionAndSendSms.ts
@@ -1,6 +1,7 @@
 import 'source-map-support/register';
 const logTransactionAndSendSms = async (event) => {
   console.log('EVENT inside log transaction and send sms: ', event);
+  console.log('HELLO WORLD');
 };
 
 export const handler = logTransactionAndSendSms;
